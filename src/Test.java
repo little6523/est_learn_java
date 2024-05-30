3daa9ba87966d771cb522ac87456d0a40bd57868
hyeonjun.lee <little6523@naver.com>
2024-05-30 14:05:31 +0900

    [ESTsoft 오르미 5기 백엔드] 09:00 ~ 14:00: Java 변수 강의 실습

--- src/Test.java
+++ src/Test.java
@@ -1,25 +1,49 @@
 public class Test {
+  public static void main(String[] args) {
+    //    int a = 20;
+    //    int b = 30;
+    //    int c = 50;
+    //
+    //    System.out.println(a + b + c);
 
-    /**
-    * @author 굿
-     * @deprecated
-     * @hidden
-    **/
+    // 정수형 자료형
+//    in1;
+//    byte a = e b = 2;
+//    long c = 3;
+//    short d = 4;
+//    // 실수형 자료형
+//    float e = 0.1f;
+//    double f = 0.2;
 
-//    한 줄 주석입니다.
-    /*
-    * 엔터를
-    * 누를 때마다
-    * 주석이
-    * 늘어나요
-    * */
-    public static void main(String[] args) {
-        int b = 10;
-        System.out.println(b);
-        System.out.println(b);
-        System.out.println(b);
-        System.out.println(b);
-        System.out.println(b);
-        System.out.println(b);
-    }
+//    char a = 'a';
+//    System.out.println(a);
+
+//    boolean isTrue = true;
+//    boolean isFalse = false;
+//    String str = "Hello World";
+
+//    int a = 123_456_789_10;   에러
+
+//    char c1 = 'A';
+//    char c2 = 65;
+//    char c3 = '\u0041';
+//
+//    char c4 = '가';
+//    char c5 = 44032;
+//    char c6 = '\uac00';
+//
+//    int uniCode = c1;
+//
+//    System.out.println(c1);
+//    System.out.println(c2);
+//    System.out.println(c3);
+//    System.out.println(c4);
+//    System.out.println(c5);
+//    System.out.println(c6);
+//    System.out.println(uniCode);
+
+//    short sVal = 10;
+//    short result = (short)(sVal + 10);
+//    System.out.println(result);
+  }
 }
