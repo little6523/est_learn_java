97973be41b29f303b6871d299bacfea4ae2e62af
hyeonjun.lee <little6523@naver.com>
2024-05-30 15:55:37 +0900

    [ESTsoft 오르미 5기 백엔드] 15:00 ~ 16:00: Java 참조변수 String 학습

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,9 @@
-public class Test implements TestInterface {
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.List;
+
+public class Test {
   public static void main(String[] args) {
     //    int a = 20;
     //    int b = 30;
@@ -7,64 +12,76 @@ public class Test implements TestInterface {
     //    System.out.println(a + b + c);
 
     // 정수형 자료형
-//    in1;
-//    byte a = e b = 2;
-//    long c = 3;
-//    short d = 4;
-//    // 실수형 자료형
-//    float e = 0.1f;
-//    double f = 0.2;
+    //    in1;
+    //    byte a = e b = 2;
+    //    long c = 3;
+    //    short d = 4;
+    //    // 실수형 자료형
+    //    float e = 0.1f;
+    //    double f = 0.2;
 
-//    char a = 'a';
-//    System.out.println(a);
+    //    char a = 'a';
+    //    System.out.println(a);
 
-//    boolean isTrue = true;
-//    boolean isFalse = false;
-//    String str = "Hello World";
+    //    boolean isTrue = true;
+    //    boolean isFalse = false;
+    //    String str = "Hello World";
 
-//    int a = 123_456_789_10;   에러
+    //    int a = 123_456_789_10;   에러
 
-//    char c1 = 'A';
-//    char c2 = 65;
-//    char c3 = '\u0041';
+    //    char c1 = 'A';
+    //    char c2 = 65;
+    //    char c3 = '\u0041';
+    //
+    //    char c4 = '가';
+    //    char c5 = 44032;
+    //    char c6 = '\uac00';
+    //
+    //    int uniCode = c1;
+    //
+    //    System.out.println(c1);
+    //    System.out.println(c2);
+    //    System.out.println(c3);
+    //    System.out.println(c4);
+    //    System.out.println(c5);
+    //    System.out.println(c6);
+    //    System.out.println(uniCode);
+
+    //    short sVal = 10;
+    //    short result = (short)(sVal + 10);
+    //    System.out.println(result);
+
+    //    float myFloat = 9.75f;
+    //
+    //    float anotherFloat = myFloat * 2;
+    //
+    ////    System.out.println("Original Float Value: " + myFloat);
+    //    System.out.println("Original Float Value: " + (int)myFloat);
+    ////    System.out.println("Calculated Float Value: " + anotherFloat);
+    //    System.out.println("Calculated Float Value: " + (byte)anotherFloat);
+
+    //    boolean isTrue = false;
+    //
+    //    if (isTrue) {
+    //      System.out.println("이건트루입니다.");
+    //    } else {
+    //      System.out.println("이건펄스입니다.");
+
+//    int[] arr = {1, 2, 3, 4, 5};
+//    int[] arr2 = new int[5];
+//    List<Integer> list = Arrays.asList(1);
+//    System.out.println(list.get(0));
 //
-//    char c4 = '가';
-//    char c5 = 44032;
-//    char c6 = '\uac00';
-//
-//    int uniCode = c1;
-//
-//    System.out.println(c1);
-//    System.out.println(c2);
-//    System.out.println(c3);
-//    System.out.println(c4);
-//    System.out.println(c5);
-//    System.out.println(c6);
-//    System.out.println(uniCode);
+//    System.out.println(arr2[0]);
 
-//    short sVal = 10;
-//    short result = (short)(sVal + 10);
-//    System.out.println(result);
-
-//    float myFloat = 9.75f;
-//
-//    float anotherFloat = myFloat * 2;
-//
-////    System.out.println("Original Float Value: " + myFloat);
-//    System.out.println("Original Float Value: " + (int)myFloat);
-////    System.out.println("Calculated Float Value: " + anotherFloat);
-//    System.out.println("Calculated Float Value: " + (byte)anotherFloat);
-
-//    boolean isTrue = false;
-//
-//    if (isTrue) {
-//      System.out.println("이건트루입니다.");
-//    } else {
-//      System.out.println("이건펄스입니다.");
-    }
-
-  @Override
-  public void test() {
-
+    String[] arr = new String[3];
+    String name = "sung yeon";
+    String name2 = new String("sung yeon");
   }
+
+  //  @Override
+  //  public void test() {
+  //
+  //  }
+
 }
