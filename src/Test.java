d299b35a5fc09f32b8e8e9c4f5d71d6433ba132e
hyeonjun.lee <little6523@naver.com>
2024-05-30 09:34:34 +0900

    init

---
+++ src/Test.java
@@ -0,0 +1,25 @@
+public class Test {
+
+    /**
+    * @author 굿
+     * @deprecated
+     * @hidden
+    **/
+
+//    한 줄 주석입니다.
+    /*
+    * 엔터를
+    * 누를 때마다
+    * 주석이
+    * 늘어나요
+    * */
+    public static void main(String[] args) {
+        int b = 10;
+        System.out.println(b);
+        System.out.println(b);
+        System.out.println(b);
+        System.out.println(b);
+        System.out.println(b);
+        System.out.println(b);
+    }
+}
