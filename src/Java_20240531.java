b0e361b308d62b321780824e652002153aa7af27
hyeonjun.lee <little6523@naver.com>
2024-05-31 15:53:15 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-05-31 강의 시간: 16:00 - 실습 코드 Java_20240531 클래스로 이동 - Test클래스(실습코드) 비움

---
+++ src/Java_20240531.java
@@ -0,0 +1,163 @@
+public class Java_20240531 {
+    private void learn() {
+        // --------------------------------------------실습 1--------------------------------
+        //    int a = 10;
+        //
+        //    String firstName = "Hyeon jun";
+        //    String secondName = "이";
+        //    String phone = "galaxy";
+        //    String status = "happy";
+        //    System.out.println("나는 " + secondName + " " + firstName + "입니다.");
+        //    System.out.println("내 폰은 " + phone + "이고, 지금 나는" + status + "합니다.");
+
+        //    System.out.println(firstName.length());
+
+        //    System.out.println(firstName == secondName);
+        // --------------------------------------------실습 1--------------------------------
+        // --------------------------------------------실습 2--------------------------------
+        //    String a = "hello";
+        //    String b = new String("hello");
+        //
+        //    System.out.println(a.equals(b));
+        //    System.out.println(a == b);
+        // --------------------------------------------실습 2--------------------------------
+        // --------------------------------------------실습 3--------------------------------
+        //    String a = "Hello World!";
+        //    System.out.println(a.indexOf("Hello"));
+        //    System.out.println(a.indexOf("g"));
+        //    System.out.println(a.indexOf("l"));
+        // --------------------------------------------실습 3--------------------------------
+        // --------------------------------------------실습 4--------------------------------
+        //    String a = "Hello World!";
+        //    System.out.println(a.contains("World"));
+        //
+        //    String b = "ESTSOFT";
+        //    System.out.println(b.contains("SOFT"));
+        // --------------------------------------------실습 4--------------------------------
+        // --------------------------------------------실습 5--------------------------------
+        //    String findChar = "What is your hobby";
+        //    System.out.println(findChar.charAt(13)+""+findChar.charAt(14)+""+
+        //            findChar.charAt(15)+""+findChar.charAt(16)+""+findChar.charAt(17));
+        // --------------------------------------------실습 5--------------------------------
+        // --------------------------------------------실습 6--------------------------------
+        //    String a = "Hello World!";
+        //    System.out.println(a.replaceAll("World", "Java"));
+        //
+        //    String b = "Hello World! Hello World! Hello World!";
+        //    System.out.println(b.replaceAll("World", "Java"));
+        //    System.out.println(b.replaceFirst("World", "Java"));
+        // --------------------------------------------실습 6--------------------------------
+        // --------------------------------------------실습 7--------------------------------
+        //    String a = "WEEEE ASKE ;ND";
+        //    System.out.println(a.substring(0, 3) + a.substring(8, 10) + a.substring(12, 14));
+        // --------------------------------------------실습 7--------------------------------
+        // --------------------------------------------실습 8--------------------------------
+        //    String a = "a:b:c:d";
+        //
+        //    String[] split = a.split(":");
+        // --------------------------------------------실습 8--------------------------------
+        // --------------------------------------------실습 9--------------------------------
+        //    String result = "Hello";
+        //    System.out.println(result + "!");
+        //    String concat = result.concat("!");
+        //    System.out.println(concat);
+        // --------------------------------------------실습 9--------------------------------
+        // --------------------------------------------실습 10--------------------------------
+        //    String name1 = "Tim";
+        //    int age1 = 30;
+        //
+        //    String name2 = "Anna";
+        //    int age2 = 45;
+        //
+        //    String format = String.format("%s의 나이는 %s세 입니다", name1, age1);
+        //    String format1 = String.format("%s의 나이는 %s세 입니다", name2, age2);
+        //    System.out.println(format);
+        //    System.out.println(format1);
+
+        // --------------------------------------------실습 10--------------------------------
+        //    String result = "";
+        //    result += "Hello ";
+        //    result += "java";
+        //    result += "World";
+        //    System.out.println(result);
+        //
+        //    StringBuilder sb = new StringBuilder();
+        //    sb.append("Hello ");
+        //    sb.append("java");
+        //    sb.append("World");
+        //    System.out.println(sb);
+        //
+        //    StringBuffer sf = new StringBuffer();
+        //    sf.append("Hello ");
+        //    sf.append("java");
+        //    sf.append("World");
+        //    System.out.println(sf);
+        // --------------------------------------------실습 10--------------------------------
+        // --------------------------------------------실습 11--------------------------------
+        //    StringBuffer sb = new StringBuffer();
+        //    sb.append("Hello");
+        //    sb.insert(0, "World");
+        //    sb.insert(0, "World");
+        //    sb.insert(0, "World");
+        //    sb.insert(0, "World");
+        //    System.out.println(sb);
+        // --------------------------------------------실습 11--------------------------------
+        // --------------------------------------------실습 12--------------------------------
+        //    int[] arr = new int[100];
+        //    System.out.println(arr);
+        //
+        //    int[] arr2 = new int[1000];
+        //
+        //    String[] arr3 = new String[100];
+        //    System.out.println(arr3[0]);
+        // --------------------------------------------실습 12--------------------------------
+        // --------------------------------------------실습 13--------------------------------
+        //    int positive = 10;
+        //    int negative = -10;
+        //
+        //    System.out.println("양의 부호 연산 적용: "+ (+positive));
+        //    System.out.println("음의 부호 연산 적용: "+ (-positive));
+        //    System.out.println("음의 부호 연산자로 부호 반전: "+ (+negative));
+        //    System.out.println("음의 부호 연산자로 다시 부호 반전: "+ (-negative));
+        // --------------------------------------------실습 13--------------------------------
+        // --------------------------------------------실습 14--------------------------------
+        //    int x = 5;
+        //    int y = 5;
+        //
+        //    System.out.println("전위 증가 연산자: " + (++x));
+        //    System.out.println("후위 증가 연산자: " + (y++));
+        //
+        //    System.out.println("==================");
+        //    System.out.println("x는 " + x);
+        //    System.out.println("y는 " + y);
+        //
+        //    System.out.println("==================");
+
+        //    int a = 100;
+        //    int b = 100;
+        //
+        //    System.out.println("전위 감소 연산자: " + (--a));
+        //    System.out.println("후위 감소 연산자: " + (b--));
+        //    System.out.println("전위 감소 연산자: " + (--a));
+        //    System.out.println("후위 감소 연산자: " + (b--));
+        //    System.out.println("전위 감소 연산자: " + (--a));
+        //    System.out.println("후위 감소 연산자: " + (b--));
+        // --------------------------------------------실습 14--------------------------------
+        //    boolean isRaining = true;
+        //
+        //    if (isRaining) {
+        //      System.out.println("하늘에서 비가내려와요");
+        //    }
+        //    if (!isRaining) {
+        //      System.out.println("김태희가 안옵니다");
+        //    }
+        //
+        //    boolean isSunny = false;
+        //
+        //    if (!isSunny) {
+        //      System.out.println("날씨가 맑아요");
+        //    } else {
+        //      System.out.println("집에서 쉬어야죠.");
+        //    }
+    }
+}
