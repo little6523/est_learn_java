993e8bc778861b5bc000fbd64b825ea4dd587a72
hyeonjun.lee <little6523@naver.com>
2024-05-31 11:42:27 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-05-31 강의 시간: 11:00 ~ 12:00 강의 내용 - Java String   - format()

--- src/Test.java
+++ src/Test.java
@@ -62,11 +62,23 @@ public class Test {
     //    String[] split = a.split(":");
     // --------------------------------------------실습 8--------------------------------
     // --------------------------------------------실습 9--------------------------------
-    String result = "Hello";
-    System.out.println(result + "!");
-    String concat = result.concat("!");
-    System.out.println(concat);
+    //    String result = "Hello";
+    //    System.out.println(result + "!");
+    //    String concat = result.concat("!");
+    //    System.out.println(concat);
     // --------------------------------------------실습 9--------------------------------
+    // --------------------------------------------실습 10--------------------------------
+    String name1 = "Tim";
+    int age1 = 30;
+
+    String name2 = "Anna";
+    int age2 = 45;
+
+    String format = String.format("%s의 나이는 %s세 입니다", name1, age1);
+    String format1 = String.format("%s의 나이는 %s세 입니다", name2, age2);
+    System.out.println(format);
+    System.out.println(format1);
+    // --------------------------------------------실습 10--------------------------------
 
   }
 }
