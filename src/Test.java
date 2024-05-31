4e8e898bbd31ab32f86542386e981be04320b348
hyeonjun.lee <little6523@naver.com>
2024-05-31 14:00:58 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-05-31 강의 시간: 13:00 ~ 14:00 강의 내용 - Java String   - StringBulider   - StringBuffer   - insert()   - 배열

--- src/Test.java
+++ src/Test.java
@@ -68,17 +68,57 @@ public class Test {
     //    System.out.println(concat);
     // --------------------------------------------실습 9--------------------------------
     // --------------------------------------------실습 10--------------------------------
-    String name1 = "Tim";
-    int age1 = 30;
+    //    String name1 = "Tim";
+    //    int age1 = 30;
+    //
+    //    String name2 = "Anna";
+    //    int age2 = 45;
+    //
+    //    String format = String.format("%s의 나이는 %s세 입니다", name1, age1);
+    //    String format1 = String.format("%s의 나이는 %s세 입니다", name2, age2);
+    //    System.out.println(format);
+    //    System.out.println(format1);
 
-    String name2 = "Anna";
-    int age2 = 45;
-
-    String format = String.format("%s의 나이는 %s세 입니다", name1, age1);
-    String format1 = String.format("%s의 나이는 %s세 입니다", name2, age2);
-    System.out.println(format);
-    System.out.println(format1);
     // --------------------------------------------실습 10--------------------------------
+    //    String result = "";
+    //    result += "Hello ";
+    //    result += "java";
+    //    result += "World";
+    //    System.out.println(result);
+    //
+    //    StringBuilder sb = new StringBuilder();
+    //    sb.append("Hello ");
+    //    sb.append("java");
+    //    sb.append("World");
+    //    System.out.println(sb);
+    //
+    //    StringBuffer sf = new StringBuffer();
+    //    sf.append("Hello ");
+    //    sf.append("java");
+    //    sf.append("World");
+    //    System.out.println(sf);
+    // --------------------------------------------실습 10--------------------------------
+    // --------------------------------------------실습 11--------------------------------
+    //    StringBuffer sb = new StringBuffer();
+    //    sb.append("Hello");
+    //    sb.insert(0, "World");
+    //    sb.insert(0, "World");
+    //    sb.insert(0, "World");
+    //    sb.insert(0, "World");
+    //    System.out.println(sb);
+    // --------------------------------------------실습 11--------------------------------
+    // --------------------------------------------실습 12--------------------------------
+    //    int[] arr = new int[100];
+    //    System.out.println(arr);
+    //
+    //    int[] arr2 = new int[1000];
+    //
+    //    String[] arr3 = new String[100];
+    //    System.out.println(arr3[0]);
+    // --------------------------------------------실습 12--------------------------------
+    // --------------------------------------------실습 13--------------------------------
+
+    // --------------------------------------------실습 13--------------------------------
 
   }
 }
