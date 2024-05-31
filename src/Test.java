51e8c8553d91ba28cef211a82d9dbff26a2a40af
hyeonjun.lee <little6523@naver.com>
2024-05-31 11:01:38 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-05-31 강의 시간: 10:00 ~ 11:00 강의 내용 - Java String   - Contains()   - charAt()   - replaceAll()   - substring()   - split()   - concat()

--- src/Test.java
+++ src/Test.java
@@ -6,34 +6,67 @@ import java.util.List;
 public class Test {
   public static void main(String[] args) {
     // --------------------------------------------실습 1--------------------------------
-//    int a = 10;
-//
-//    String firstName = "Hyeon jun";
-//    String secondName = "이";
-//    String phone = "galaxy";
-//    String status = "happy";
-//    System.out.println("나는 " + secondName + " " + firstName + "입니다.");
-//    System.out.println("내 폰은 " + phone + "이고, 지금 나는" + status + "합니다.");
+    //    int a = 10;
+    //
+    //    String firstName = "Hyeon jun";
+    //    String secondName = "이";
+    //    String phone = "galaxy";
+    //    String status = "happy";
+    //    System.out.println("나는 " + secondName + " " + firstName + "입니다.");
+    //    System.out.println("내 폰은 " + phone + "이고, 지금 나는" + status + "합니다.");
 
-//    System.out.println(firstName.length());
+    //    System.out.println(firstName.length());
 
-//    System.out.println(firstName == secondName);
+    //    System.out.println(firstName == secondName);
     // --------------------------------------------실습 1--------------------------------
     // --------------------------------------------실습 2--------------------------------
-//    String a = "hello";
-//    String b = new String("hello");
-//
-//    System.out.println(a.equals(b));
-//    System.out.println(a == b);
+    //    String a = "hello";
+    //    String b = new String("hello");
+    //
+    //    System.out.println(a.equals(b));
+    //    System.out.println(a == b);
     // --------------------------------------------실습 2--------------------------------
     // --------------------------------------------실습 3--------------------------------
-//    String a = "Hello World!";
-//    System.out.println(a.indexOf("!"));
-//    System.out.println(a.indexOf("g"));
-//    System.out.println(a.indexOf("l"));
+    //    String a = "Hello World!";
+    //    System.out.println(a.indexOf("Hello"));
+    //    System.out.println(a.indexOf("g"));
+    //    System.out.println(a.indexOf("l"));
     // --------------------------------------------실습 3--------------------------------
     // --------------------------------------------실습 4--------------------------------
-//    String a = "Hello World!";
-//    System.out.println(a.contains());
+    //    String a = "Hello World!";
+    //    System.out.println(a.contains("World"));
+    //
+    //    String b = "ESTSOFT";
+    //    System.out.println(b.contains("SOFT"));
+    // --------------------------------------------실습 4--------------------------------
+    // --------------------------------------------실습 5--------------------------------
+    //    String findChar = "What is your hobby";
+    //    System.out.println(findChar.charAt(13)+""+findChar.charAt(14)+""+
+    //            findChar.charAt(15)+""+findChar.charAt(16)+""+findChar.charAt(17));
+    // --------------------------------------------실습 5--------------------------------
+    // --------------------------------------------실습 6--------------------------------
+    //    String a = "Hello World!";
+    //    System.out.println(a.replaceAll("World", "Java"));
+    //
+    //    String b = "Hello World! Hello World! Hello World!";
+    //    System.out.println(b.replaceAll("World", "Java"));
+    //    System.out.println(b.replaceFirst("World", "Java"));
+    // --------------------------------------------실습 6--------------------------------
+    // --------------------------------------------실습 7--------------------------------
+    //    String a = "WEEEE ASKE ;ND";
+    //    System.out.println(a.substring(0, 3) + a.substring(8, 10) + a.substring(12, 14));
+    // --------------------------------------------실습 7--------------------------------
+    // --------------------------------------------실습 8--------------------------------
+    //    String a = "a:b:c:d";
+    //
+    //    String[] split = a.split(":");
+    // --------------------------------------------실습 8--------------------------------
+    // --------------------------------------------실습 9--------------------------------
+    String result = "Hello";
+    System.out.println(result + "!");
+    String concat = result.concat("!");
+    System.out.println(concat);
+    // --------------------------------------------실습 9--------------------------------
+
   }
 }
