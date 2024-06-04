c1d1a16add1071c4274cb4a205eb115a62a07014
hyeonjun.lee <little6523@naver.com>
2024-06-04 09:53:33 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-04 강의 시간: 09:00 ~ 10:00 강의 내용 - while문 실습

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,41 @@ import java.util.List;
 
 public class Test {
   public static void main(String[] args) {
+    // --------------------------------------------실습 1--------------------------------
+    //    int i = 1;
+    //    while (i <= 5) {
+    //      System.out.println("현재 i의 값은 = "+ i);
+    //      i++;
+    //    }
 
+    //    int i = 10;
+    //    while (i >= 0) {
+    //      System.out.println("현재 카운트 : "+ i);
+    //      i--;
+    //    }
+    //    System.out.println("카운트 종료");
+
+//    int i = 1;
+//    int res = 0;
+//    int n = 100;
+//
+//    while (i <= n) {
+//      res += i;
+//      i += 1;
+//    }
+//    System.out.println(res);
+
+    int[] numbers = {5, 2, 9, 1, 7, 4, 6, 3, 8};
+    int sum = 0;
+    int i = 0;
+    while (i < numbers.length) {
+      sum += numbers[i];
+      i++;
+    }
+    System.out.println(sum/numbers.length);
+    // --------------------------------------------실습 1--------------------------------
+    // --------------------------------------------실습 2--------------------------------
+
+    // --------------------------------------------실습 2--------------------------------
   }
 }
