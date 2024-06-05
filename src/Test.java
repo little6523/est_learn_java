91ecd5eb1ed23866e257dafe0acd0836ef2563a8
hyeonjun.lee <little6523@naver.com>
2024-06-05 11:00:40 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-05 강의 시간: 10:00 ~ 11:00 강의 내용 - Scanner

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,25 @@
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.function.IntUnaryOperator;
-import java.util.stream.IntStream;
+import java.util.Scanner;
 
 public class Test {
   public static void main(String[] args) {
-    String[] words = {"apple", "banana", "cherry", "durian"};
-    for (int i = 0; i < words.length - 1; i++) {
-      System.out.println(i);
-      for (int j = i + 1; j < words.length; j++) {
-        System.out.println(words[i] + " " + words[j]);
-      }
-    }
+    // --------------------------------------------실습 1--------------------------------
+    //    String[] words = {"apple", "banana", "cherry", "durian"};
+    //    for (int i = 0; i < words.length - 1; i++) {
+    //      System.out.println(i);
+    //      for (int j = i + 1; j < words.length; j++) {
+    //        System.out.println(words[i] + " " + words[j]);
+    //      }
+    //    }
+    // --------------------------------------------실습 1--------------------------------
+    // --------------------------------------------실습 2--------------------------------
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("정수를 입력해주세요.");
+    int number = sc.nextInt();
+
+    System.out.println("제가 입력한 값은 " + number + "입니다.");
+
+    sc.close();
+    // --------------------------------------------실습 2--------------------------------
   }
 }
