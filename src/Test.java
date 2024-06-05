015d8cd01aeec5eec8156fb246cce5e1580711ca
hyeonjun.lee <little6523@naver.com>
2024-06-05 14:09:51 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-05 강의 시간: 13:00 ~ 14:00 강의 내용 - random

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Test {
   public static void main(String[] args) {
@@ -37,36 +38,59 @@ public class Test {
     //
     //    sc.close();
 
-    /*
-    * "계절을 맞춰보세요!"라는 자바 프로그램을 작성해보세요.
-사용자에게 1에서 4 사이의 숫자를 입력하라고 요청합니다.
-각 숫자는 계절을 나타냅니다: 1은 봄, 2는 여름, 3은 가을, 4는 겨울입니다.
-사용자가 숫자를 입력하면, 프로그램은 해당 계절의 이름을 출력해야 합니다. 만약 사용자가 1에서 4 사이의 숫자가 아닌 것을 입력하면, "잘못된 입력입니다!"라고 출력해야 합니다.
-이 문제를 해결하기 위해 switch 문을 사용하세요.
-    * */
-
-    Scanner sc = new Scanner(System.in);
-    System.out.println("1에서 4사이의 숫자를 입력하세요.");
-
-    int weather = sc.nextInt();
-    sc.nextLine();
-    switch (weather) {
-      case 1:
-        System.out.println("봄");
-        break;
-      case 2:
-        System.out.println("여름");
-        break;
-      case 3:
-        System.out.println("가을");
-        break;
-      case 4:
-        System.out.println("겨울");
-        break;
-      default:
-        System.out.println("잘못된 입력입니다!");
-    }
-    sc.close();
+    //    Scanner sc = new Scanner(System.in);
+    //    System.out.println("1에서 4사이의 숫자를 입력하세요.");
+    //
+    //    int weather = sc.nextInt();
+    //    sc.nextLine();
+    //    switch (weather) {
+    //      case 1:
+    //        System.out.println("봄");
+    //        break;
+    //      case 2:
+    //        System.out.println("여름");
+    //        break;
+    //      case 3:
+    //        System.out.println("가을");
+    //        break;
+    //      case 4:
+    //        System.out.println("겨울");
+    //        break;
+    //      default:
+    //        System.out.println("잘못된 입력입니다!");
+    //    }
+    //    sc.close();
     // --------------------------------------------실습 3--------------------------------
+    // --------------------------------------------실습 4--------------------------------
+    //    Random random = new Random();
+    //
+    //    int i = random.nextInt(6)+1;
+    //
+    //    System.out.println(i);
+
+    Random random = new Random();
+    ArrayList<Integer> lotto = new ArrayList<>();
+    while (lotto.size() < 6) {
+      int num = random.nextInt(45) + 1;
+      if (!lotto.contains(num)){
+        lotto.add(num);
+      }
+    }
+    Collections.sort(lotto);
+    System.out.print("로또 번호: ");
+    for (int num : lotto) {
+      System.out.print(num + " ");
+    }
+
+    // int -> Integer : boxing
+    // Integer -> int : unboxing
+
+    // --------------------------------------------실습 4--------------------------------
+    // --------------------------------------------실습 5--------------------------------
+
+    // --------------------------------------------실습 5--------------------------------
+    // --------------------------------------------실습 6--------------------------------
+
+    // --------------------------------------------실습 6--------------------------------
   }
 }
