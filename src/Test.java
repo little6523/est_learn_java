982068bd0343ee844609efb27526c428ced14d67
hyeonjun.lee <little6523@naver.com>
2024-06-05 09:49:01 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-05 강의 시간: 09:00 ~ 10:00 강의 내용 - for문

--- src/Test.java
+++ src/Test.java
@@ -7,6 +7,12 @@ import java.util.stream.IntStream;
 
 public class Test {
   public static void main(String[] args) {
-
+    String[] words = {"apple", "banana", "cherry", "durian"};
+    for (int i = 0; i < words.length - 1; i++) {
+      System.out.println(i);
+      for (int j = i + 1; j < words.length; j++) {
+        System.out.println(words[i] + " " + words[j]);
+      }
+    }
   }
 }
