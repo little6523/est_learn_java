3231a32fea792e7217569cb095ecd734984fb252
hyeonjun.lee <little6523@naver.com>
2024-06-11 10:00:43 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-11 강의 시간: 09:00 ~ 10:00 강의 내용 - 메소드

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,33 @@ import java.util.*;
 
 public class Test {
   public static void main(String[] args) {
+    int okay = okay(1, 2);
 
+  }
+
+  static int okay(int a, int b) {
+    System.out.println(a + b);
+
+    return 1;
+  }
+
+  static int add(int a, int b) {
+    return a + b;
+  }
+
+  static int sub(int a, int b) {
+    return a - b;
+  }
+
+  static int mul(int a, int b) {
+    return a * b;
+  }
+
+  static int div(int a, int b) {
+    return a / b;
+  }
+
+  static void login(String email, String password) {
 
   }
 }
