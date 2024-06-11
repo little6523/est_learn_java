e8362c0acbeacb66339aeee2c9121bd276eeb5dd
hyeonjun.lee <little6523@naver.com>
2024-06-11 11:11:14 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-11 강의 시간: 10:00 ~ 11:00 강의 내용 - 접근제어자 - static

--- src/Test.java
+++ src/Test.java
@@ -5,33 +5,47 @@ import java.util.*;
 
 public class Test {
   public static void main(String[] args) {
-    int okay = okay(1, 2);
+    // -------------------------- 1교시 ------------------------------
+    //    int okay = okay(1, 2);
+    // -------------------------- 1교시 ------------------------------
+    // -------------------------- 2교시 ------------------------------
+    //    Test2 test2 = new Test2();
+    //    System.out.println(test2.b);
+    //    System.out.println(test2.good());
+    Test2.call();
+    Test2 test2 = new Test2();
+    // -------------------------- 2교시 ------------------------------
 
   }
 
-  static int okay(int a, int b) {
-    System.out.println(a + b);
-
-    return 1;
-  }
-
-  static int add(int a, int b) {
-    return a + b;
-  }
-
-  static int sub(int a, int b) {
-    return a - b;
-  }
-
-  static int mul(int a, int b) {
-    return a * b;
-  }
-
-  static int div(int a, int b) {
-    return a / b;
-  }
-
-  static void login(String email, String password) {
-
-  }
+  // -------------------------- 1교시 ------------------------------
+  //  static int okay(int a, int b) {
+  //    System.out.println(a + b);
+  //
+  //    return 1;
+  //  }
+  //
+  //  static int add(int a, int b) {
+  //    return a + b;
+  //  }
+  //
+  //  static int sub(int a, int b) {
+  //    return a - b;
+  //  }
+  //
+  //  static int mul(int a, int b) {
+  //    return a * b;
+  //  }
+  //
+  //  static int div(int a, int b) {
+  //    return a / b;
+  //  }
+  //
+  //  static void login(String email, String password) {
+  //
+  //  }
+  // -------------------------- 1교시 ------------------------------
+  // -------------------------- 2교시 ------------------------------
+  // Test2 클래스 작성
+  // -------------------------- 2교시 ------------------------------
 }
---
+++ src/Test2.java
@@ -0,0 +1,18 @@
+public class Test2 {
+
+  public static void call(){
+    System.out.println("call");
+  }
+
+  private int a = 1;
+
+  public String b = "hello";
+
+  public String good() {
+    return "okay";
+  }
+
+  private void add() {
+    System.out.println("add");
+  }
+}
