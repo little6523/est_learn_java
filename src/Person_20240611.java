6a5caea4c762ede44de9bbe9892e016a555c84be
hyeonjun.lee <little6523@naver.com>
2024-06-12 09:52:51 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-12 강의 시간: 09:00 ~ 10:00 강의 내용 - 어제 실습 코드 파일 정리

---
+++ src/Person_20240611.java
@@ -0,0 +1,31 @@
+public class Person_20240611 {
+
+  String name;
+  int phoneNumber;
+  int money;
+  int age;
+
+  // 생성자
+  public Person_20240611(String name, int phoneNumber, int money, int age) {
+    this.name = name;
+    this.phoneNumber = phoneNumber;
+    this.money = money;
+    this.age = age;
+  }
+
+  public void sayHello() {
+    System.out.println("My name is " + name);
+  }
+
+  public void sayMoney() {
+      System.out.println("내가 가지고 있는 돈은 " + money * 10000 + "원이야");
+  }
+
+  public void teaching(Student_20240611 student) {
+    student.levelUp();
+  }
+
+  public void cheating(Student_20240611 student) {
+    student.levelDown();
+  }
+}
