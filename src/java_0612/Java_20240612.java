ec5864aa79298ba608c22cc70aa58c7c18db504e
hyeonjun.lee <little6523@naver.com>
2024-06-13 09:30:54 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-13 강의 시간: 09:00 강의 내용 - 실습 코드 패키지 구조 개선

---
+++ src/java_0612/Java_20240612.java
@@ -0,0 +1,54 @@
+package java_0612;
+
+public class Java_20240612 {
+
+    public void learn() {
+        // ------------------------- 1교시 -------------------------
+        //    Person_20240612 person1 = new p1.Person_20240612("승조", 25);
+        //    Person_20240612 person2 = new p1.Person_20240612("max", 21);
+        //
+        //    person1.sayHello();
+        //    person1.introduce();
+        //
+        //    person2.sayHello();
+        //    person2.introduce();
+
+        // ------------------------- 1교시 -------------------------
+        // ------------------------- 2교시 -------------------------
+        //    Person_20240612.gender = "adf"; // 변경불가
+        // ------------------------- 2교시 -------------------------
+        // ------------------------- 3교시 -------------------------
+        //    Person_20240612 person  = new Person_20240612("D", 25, "test");
+        //    Person_20240612 person1 = new Person_20240612("D", 25, "test");
+        //    Person_20240612 person2 = new Person_20240612("D", 25, "test");
+        //    Person_20240612 person3 = new Person_20240612("D", 25, "test");
+        //    Person_20240612 person4 = new Person_20240612("D", 25, "test");
+        //    Person_20240612 person5 = new Person_20240612("D", 25, "test");
+        //    Person_20240612 person6 = new Person_20240612("D", 25, "test");
+        //    Person_20240612 person7 = new Person_20240612("D", 25, "test");
+        //    Person_20240612 person8 = new Person_20240612("D", 25, "test");
+        //    // 생성자를 통해 값을 주입할 수 있는 것은 상수가 아니다.
+        //
+        //    Person_20240612 okay = new Person_20240612("okay", 25, "test");
+        //    System.out.println(okay.name2);
+        // ------------------------- 3교시 -------------------------
+        // ------------------------- 4교시 -------------------------
+
+        //    HouseKim_20240612 houseKim = new HouseKim_20240612();
+        ////    houseKim.
+        //
+        //    Sonata sonata = new Sonata();
+        //    System.out.println(sonata.company); // 에러
+
+        // ------------------------- 4교시 -------------------------
+        // ------------------------- 5교시 -------------------------
+        //    Car_20240612 car = new Car_20240612();
+        //    car.setSpeed(-100);
+        //    car.checkSpeed();
+        // ------------------------- 5교시 -------------------------
+        // ------------------------- 6교시 -------------------------
+//        Car_20240612 car = new Car_20240612();
+//        System.out.println(car.getSpeed());
+        // ------------------------- 6교시 -------------------------
+    }
+}
