4b902a736481b02068bbb479480e3b4124fd4cef
hyeonjun.lee <little6523@naver.com>
2024-06-18 15:50:39 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-18 강의 시간: 15:00 ~ 16:00 강의 내용 - ArrayList - LinkedList

--- src/Test.java
+++ src/Test.java
@@ -60,27 +60,70 @@ public class Test {
     // ----------------- 4교시 ---------------------------
     // ----------------- 5교시 ---------------------------
 
-    List<String> arraylist = new ArrayList<>();
-    arraylist.add("Honggildong");
-    arraylist.add("Honggildong2");
-    arraylist.add("Honggildong2");
-    arraylist.add("Honggildong2");
-    arraylist.add("Honggildong2");
-    arraylist.add("Honggildong2");
-
-    arraylist.remove("홍길동2");
-
-    arraylist.removeIf((s)-> s.equals("홍길동2"));
-
-    for (int i = 0; i < arraylist.size(); i++) {
-      System.out.println(arraylist.get(i));
-    }
-    System.out.println("========================");
-    for (String str : arraylist) {
-      System.out.println(str);
-    }
+    //    List<String> arraylist = new ArrayList<>();
+    //    arraylist.add("Honggildong");
+    //    arraylist.add("Honggildong2");
+    //    arraylist.add("Honggildong2");
+    //    arraylist.add("Honggildong2");
+    //    arraylist.add("Honggildong2");
+    //    arraylist.add("Honggildong2");
+    //
+    //    arraylist.remove("홍길동2");
+    //
+    //    arraylist.removeIf((s)-> s.equals("홍길동2"));
+    //
+    //    for (int i = 0; i < arraylist.size(); i++) {
+    //      System.out.println(arraylist.get(i));
+    //    }
+    //    System.out.println("========================");
+    //    for (String str : arraylist) {
+    //      System.out.println(str);
+    //    }
 
     // ----------------- 5교시 ---------------------------
+    // ----------------- 6교시 ---------------------------
+
+    //    List<String> arrayList = new ArrayList<>();
+    //    arrayList.add("Java");
+    //    arrayList.add("Spring");
+    //    arrayList.add("Servlet/JSP");
+    //    arrayList.add("DBMS");
+    //    arrayList.add("JPA");
+    //
+    //    System.out.println("총 객체수 : " + arrayList.size());
+    //
+    //    System.out.println("================");
+    //
+    //    System.out.println("두 번째 인덱스 : " + arrayList.get(2));
+    //
+    //    System.out.println(arrayList.contains("java"));
+    //
+    //    ArrayList<Object> objects = new ArrayList<>();
+    //
+    //    objects.add(null); // null값도 값이다.
+    //
+    //    System.out.println(objects.isEmpty());
+    //    objects.clear();
+    //    System.out.println(objects.isEmpty());
+
+    List<String> arrayList = new ArrayList<>();
+    List<String> linkedList = new LinkedList<>();
+
+    long startTime, endTime;
+    startTime = System.nanoTime();
+    for (int i = 0; i < 10000; i++) {
+      arrayList.add(String.valueOf(i));
+    }
+    endTime = System.nanoTime();
+    System.out.println("arrayList 걸린시간: " + (endTime - startTime));
+
+    startTime = System.nanoTime();
+    for (int i = 0; i < 10000; i++) {
+      linkedList.add(0, String.valueOf(i));
+    }
+    endTime = System.nanoTime();
+    System.out.println("linkedList 걸린시간: " + (endTime - startTime));
+    // ----------------- 6교시 ---------------------------
   }
 
   public static <T extends Number> int compare(T t1, T t2) {
