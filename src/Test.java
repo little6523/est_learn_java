be047baeb88150d805c7c2b2e9b61c1ee592ce1c
hyeonjun.lee <little6523@naver.com>
2024-06-18 10:48:12 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-18 강의 시간: 10:00 ~ 11:00 강의 내용 - 제한된 타입 파라미터

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,31 @@ import java.util.Objects;
 
 public class Test {
   public static void main(String[] args) {
+    // ----------------- 2교시 ---------------------------
+    //    System.out.println(compare(1, 2));
 
+    // 메서드 이름은 compareStirng t1,t2 매개변수 반환값 void 타입 파라미터 한정자가 String
+    // 내부 로직은 문자열이 같으면 okay 출력 틀리면 no 출력
+    compareString("hello", "hello");
+    compareString("hello", "hello1");
+    // ----------------- 2교시 ---------------------------
+  }
 
+  public static <T extends Number> int compare(T t1, T t2) {
+    //    double v1 = t1.doubleValue();
+    //    double v2 = t1.doubleValue();
+    double v1 = (double) t1;
+    double v2 = (double) t2;
+    return Double.compare(v1, v2);
+  }
+
+  public static <T extends String> void compareString(String t1, String t2) {
+    //    if (t1.equals(t2)) {
+    //      System.out.println("okay");
+    //      return;
+    //    }
+    //    System.out.println("no");
+
+    System.out.println(t1.equals(t2) ? "okay" : "no");
   }
 }
