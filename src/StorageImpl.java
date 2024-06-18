143f423c7e89ad5c5be3e1bf7c8d08680cb12faa
hyeonjun.lee <little6523@naver.com>
2024-06-18 13:50:44 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-18 강의 시간: 13:00 ~ 14:00 강의 내용 - 제네릭 타입의 상속과 구현

---
+++ src/StorageImpl.java
@@ -0,0 +1,18 @@
+public class StorageImpl<T> implements Storage<T>{
+
+    private T[] array;
+
+    public StorageImpl(int capacity) {
+        this.array = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T item, int index) {
+        this.array[index] = item;
+    }
+
+    @Override
+    public T get(int index) {
+        return array[index];
+    }
+}
