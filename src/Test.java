96d41d2858424e08da90e1d5f81c9fbc184697e5
hyeonjun.lee <little6523@naver.com>
2024-06-19 10:14:25 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-19 강의 시간: 09:00 ~ 10:00 강의 내용 - Set - Iterator

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,48 @@ import java.util.*;
 
 public class Test {
   public static void main(String[] args) {
+    Set<Integer> integerSet = new HashSet<>();
 
+    integerSet.add(1);
+    integerSet.add(2);
+    integerSet.add(3);
+    integerSet.add(4);
+    integerSet.add(5);
+
+    Iterator<Integer> iterator = integerSet.iterator();
+
+    while(iterator.hasNext()) {
+      iterator.next();
+      iterator.remove();
+    }
+
+    while(iterator.hasNext()) {
+      System.out.println(iterator.next());
+    }
+
+    Set<String> stringsSet = new HashSet<>();
+
+    stringsSet.add("Java");
+    stringsSet.add("Spring");
+    stringsSet.add("JSP");
+    stringsSet.add("Java");
+    stringsSet.add("DBMS");
+
+    System.out.println(stringsSet.size());
+
+    stringsSet.remove("Java");
+
+    Iterator<String> iterator2 = stringsSet.iterator();
+    while(iterator2.hasNext()) {
+      System.out.println(iterator2.next());
+    }
+
+    stringsSet.clear();
+
+    if(stringsSet.isEmpty()) {
+      System.out.println("비었습니다.");
+    } else {
+      System.out.println("비지 않았습니다.");
+    }
   }
 }
