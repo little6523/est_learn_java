d50c79ff268467d6c1e9be889d88a34b9c8f761d
hyeonjun.lee <little6523@naver.com>
2024-06-19 10:59:51 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-19 강의 시간: 10:00 ~ 11:00 강의 내용 - Set

---
+++ src/Member.java
@@ -0,0 +1,26 @@
+public class Member {
+  String name;
+  int age;
+
+  public Member(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  @Override
+  public int hashCode() {
+    int i = name.hashCode() + age;
+    System.out.println(i);
+    return i;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof Member) {
+      Member member = (Member) obj;
+      return member.name.equals(this.name) && member.age == this.age;
+    } else {
+      return false;
+    }
+  }
+}
