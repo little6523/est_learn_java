c0d038b455553e39c453c60b30f9604c849b1f54
hyeonjun.lee <little6523@naver.com>
2024-06-19 13:49:58 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-19 강의 시간: 13:00 ~ 14:00 강의 내용 - Map - Entry

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,3 @@
-import java_0618.Course;
-import java_0618.Student;
-import java_0618.Worker;
-
 import java.util.*;
 
 public class Test {
@@ -63,31 +59,108 @@ public class Test {
     // ----------------- 2교시 -------------------------
     // ----------------- 3교시 -------------------------
 
-    Map<String, Integer> map = new HashMap<>();
-
-    map.put("홍길동", 30);
-    map.put("홍길동", 40);
-
-    System.out.println(map.get("홍길동"));
-    System.out.println(map.remove("홍길동"));
-
-    Map<String, List<String>> map2 = new HashMap<>();
-    List<String> stringList = new ArrayList<>();
-
-    stringList.add("홍길동");
-    stringList.add("홍길동");
-    stringList.add("홍길동");
-    map2.put("홍길동", stringList);
-
-    Set<String> strings = map2.keySet();
-    Iterator<String> iterator = strings.iterator();
-
-    while (iterator.hasNext()) {
-      String next = iterator.next();
-      List<String> stringList1 = map2.get(next);
-      System.out.println(stringList);
-    }
+    //    Map<String, Integer> map = new HashMap<>();
+    //
+    //    map.put("홍길동", 30);
+    //    map.put("홍길동", 40);
+    //
+    //    System.out.println(map.get("홍길동"));
+    //    System.out.println(map.remove("홍길동"));
+    //
+    //    Map<String, List<String>> map2 = new HashMap<>();
+    //    List<String> stringList = new ArrayList<>();
+    //
+    //    stringList.add("홍길동");
+    //    stringList.add("홍길동");
+    //    stringList.add("홍길동");
+    //    map2.put("홍길동", stringList);
+    //
+    //    Set<String> strings = map2.keySet();
+    //    Iterator<String> iterator = strings.iterator();
+    //
+    //    while (iterator.hasNext()) {
+    //      String next = iterator.next();
+    //      List<String> stringList1 = map2.get(next);
+    //      System.out.println(stringList);
+    //    }
 
     // ----------------- 3교시 -------------------------
+    // ----------------- 4교시 -------------------------
+    //    Map<String, List<String>> map2 = new HashMap<>();
+    //    List<String> stringList = new ArrayList<>();
+    //
+    //    stringList.add("홍길동");
+    //    stringList.add("홍길동");
+    //    stringList.add("홍길동");
+    //    map2.put("키1", stringList);
+    //
+    //    Set<Map.Entry<String, List<String>>> entries = map2.entrySet();
+    //
+    //    for(Map.Entry<String,List<String>> entry:entries){
+    //      String key = entry.getKey();
+    //      List<String> value = entry.getValue();
+    //      System.out.println(key);
+    //      System.out.println("========");
+    //      System.out.println(value);
+    //    }
+
+    //    Map<String, Integer> map = new HashMap<>();
+    //
+    //    map.put("홍길동", 80);
+    //    map.put("카리나", 85);
+    //    map.put("제시카", 95);
+    //    map.put("홍길동", 80); // "홍길동" 키가 같기 때문에 가장 마지막에 저장한 값으로 엎어씀
+    //    System.out.println("총 Entry 수: " + map.size()); // 저장된 총 Entry 수 얻기
+    //
+    //    // 2. 객체 찾기
+    //    System.out.println("\t홍길동: " + map.get("홍길동"));
+    //    System.out.println();
+    //
+    //    // 3. 객체를 하나씩 처리
+    //    Set<String> keySet = map.keySet();
+    //    Iterator<String> iterator = keySet.iterator();
+    //    while (iterator.hasNext()) {
+    //      String key = iterator.next();
+    //      Integer value = map.get(key);
+    //      System.out.println("\t" + key + ": " + value);
+    //    }
+    //    System.out.println();
+    //
+    //    // 4. 객체 삭제
+    //    map.remove("홍길동");
+    //    System.out.println("총 Entry 수: " + map.size());
+    //
+    //    // 5. 객체를 하나씩 처리
+    //    Set<Map.Entry<String, Integer>> entrySet = map.entrySet(); // Map.EntrySet 얻기
+    //    for (Map.Entry<String, Integer> entry : entrySet) {
+    //      String key = entry.getKey();
+    //      Integer value = entry.getValue();
+    //      System.out.println("\t" + key + ": " + value);
+    //    }
+    //    System.out.println();
+    //
+    //    // 6. 모든 Map.Entry 삭제
+    //    map.clear();
+    //    System.out.println("총 Entry 수: " + map.size());
+
+    HashMap<Student, Integer> students = new HashMap<>();
+    Student student1 = new Student(1, "가나다");
+    Student student2 = new Student(2, "라마바");
+    Student student3 = new Student(3, "아자차");
+    Student student4 = new Student(4, "카타파");
+
+    students.put(student1, 100);
+    students.put(student2, 90);
+    students.put(student3, 80);
+    students.put(student4, 70);
+
+    Set<Map.Entry<Student, Integer>> studentsSet = students.entrySet();
+    for (Map.Entry<Student, Integer> entry : studentsSet) {
+      Student key = entry.getKey();
+      Integer value = entry.getValue();
+      System.out.println("\t" + key.name + ": " + value);
+    }
+
+    // ----------------- 4교시 -------------------------
   }
 }
