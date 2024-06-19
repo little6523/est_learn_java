90b5d6f908a6a60a17b4d9cac5a3f49abababc3b
hyeonjun.lee <little6523@naver.com>
2024-06-19 11:40:21 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-19 강의 시간: 11:00 ~ 12:00 강의 내용 - Map

--- src/Test.java
+++ src/Test.java
@@ -53,13 +53,41 @@ public class Test {
     // ----------------- 1교시 -------------------------
     // ----------------- 2교시 -------------------------
 
-      Set<Member> set = new HashSet<>();
-
-      set.add(new Member("홍길동", 31));
-      set.add(new Member("홍길동", 30));
-
-      System.out.println("총 객체수: " + set.size());
+    //      Set<Member> set = new HashSet<>();
+    //
+    //      set.add(new Member("홍길동", 31));
+    //      set.add(new Member("홍길동", 30));
+    //
+    //      System.out.println("총 객체수: " + set.size());
 
     // ----------------- 2교시 -------------------------
+    // ----------------- 3교시 -------------------------
+
+    Map<String, Integer> map = new HashMap<>();
+
+    map.put("홍길동", 30);
+    map.put("홍길동", 40);
+
+    System.out.println(map.get("홍길동"));
+    System.out.println(map.remove("홍길동"));
+
+    Map<String, List<String>> map2 = new HashMap<>();
+    List<String> stringList = new ArrayList<>();
+
+    stringList.add("홍길동");
+    stringList.add("홍길동");
+    stringList.add("홍길동");
+    map2.put("홍길동", stringList);
+
+    Set<String> strings = map2.keySet();
+    Iterator<String> iterator = strings.iterator();
+
+    while (iterator.hasNext()) {
+      String next = iterator.next();
+      List<String> stringList1 = map2.get(next);
+      System.out.println(stringList);
+    }
+
+    // ----------------- 3교시 -------------------------
   }
 }
