d05c8602a0409b17030cea994504df3d5206186b
hyeonjun.lee <little6523@naver.com>
2024-06-20 11:37:49 +0900

    [ESTsoft 오르미 5기 백엔드] 강의 날짜: 2024-06-20 강의 시간: 11:00 ~ 12:00 강의 내용 - Thread

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,18 @@ import java.util.*;
 
 public class Test {
   public static void main(String[] args) {
+    // -------------------- 3교시 --------------------
+    MyThread myThread = new MyThread("스레드");
+    MyThread myThread1 = new MyThread("스레드1");
+    MyThread myThread2 = new MyThread("스레드2");
+    MyThread myThread3 = new MyThread("스레드3");
+    MyThread myThread4 = new MyThread("스레드4");
 
+    myThread.start();
+    myThread1.start();
+    myThread2.start();
+    myThread3.start();
+    myThread4.start();
+    // -------------------- 3교시 --------------------
   }
 }
